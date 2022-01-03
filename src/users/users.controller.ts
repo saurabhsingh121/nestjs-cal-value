a03933643cb1b4242a16774e72e2fedb547b2c11
Saurabh Singh <saurabhsingh121@gmail.com>
2022-01-03 10:27:02 +0530

    interceptor basics

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,12 +7,16 @@ import {
   Param,
   Query,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('/signup')
@@ -20,6 +24,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  //   @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
